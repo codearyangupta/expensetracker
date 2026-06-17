d543c8aacb5088107d8f6dcff0d262a169f7db33
codearyangupta <aaryangupta2509@gmail.com>
2026-06-17 22:08:13 +0530

    connect backend to neon

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,17 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker , declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Aaryan%402509@localhost:5432/expense_db"
+
+
+DATABASE_URL = "postgresql://neondb_owner:npg_0Um5nevoDqXI@ep-shy-pine-adzlx26v.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-    autocommit = False,
-    autoflush = False,
-    bind = engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 Base = declarative_base()
-
-
